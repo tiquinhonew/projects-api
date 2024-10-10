ad1f549ea576d4848ac06aed255afec0edfe3c46
Douglas Moraes <douglas.am@gmail.com>
2024-10-09 22:05:34 -0300

    git commit -m "first commit"

---
+++ src/main/java/com/dam/orla/OrlaApiApplication.java
@@ -0,0 +1,13 @@
+package com.dam.orla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrlaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrlaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dam/orla/OrlaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.orla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrlaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
