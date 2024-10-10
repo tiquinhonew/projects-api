11f2f0dae39c8a749a8d5494929154bcde22d319
Douglas Moraes <douglas.am@gmail.com>
2024-10-10 12:12:07 -0300

    Incluindo informações referente a documentação

--- src/main/java/com/dam/orla/api/dto/request/ProjetoRequest.java
+++ src/main/java/com/dam/orla/api/dto/request/ProjetoRequest.java
@@ -5,12 +5,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 import java.util.List;
 
-public record CriarProjetoRequest(
+public record ProjetoRequest(
     @Schema(description = "Nome do projeto", example = "Projeto cartão de credito")
     String nome,
-    @Schema(description = "Data de criação do projeto", example = "2023-12-31")
+    @Schema(description = "Data de criação do projeto", example = "2023-12-31", name = "data_criacao")
     LocalDate dataCriacao,
-    @Schema(description = "Lista de ids dos funcionários que participam do projeto", example = "[42, 43, 44]")
+    @Schema(description = "Lista de ids dos funcionários que participam do projeto", example = "[42, 43, 44]", name = "funcionarios_ids")
     List<Long> funcionariosIds
 ) {
 
