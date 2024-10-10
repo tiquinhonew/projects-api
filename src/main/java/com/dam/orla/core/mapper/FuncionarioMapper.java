8236194bbb90bc140de3d7c3bd5414dfdd7ea099
Douglas Moraes <douglas.am@gmail.com>
2024-10-10 12:15:08 -0300

    Incluindo classe de mapper

--- src/main/java/com/dam/orla/core/mapper/FuncionarioMapper.java
+++ src/main/java/com/dam/orla/core/mapper/FuncionarioMapper.java
@@ -1,4 +1,37 @@
 package com.dam.orla.core.mapper;
 
+import com.dam.orla.api.dto.request.FuncionarioRequest;
+import com.dam.orla.api.dto.response.FuncionarioResponse;
+import com.dam.orla.domain.model.FuncionarioModel;
+
+import java.util.List;
+
 public class FuncionarioMapper {
+    private FuncionarioMapper() {
+    }
+
+    public static FuncionarioResponse entityToDto(FuncionarioModel funcionarioModel) {
+        return new FuncionarioResponse(
+            funcionarioModel.getId(),
+            funcionarioModel.getNome(),
+            funcionarioModel.getCpf(),
+            funcionarioModel.getEmail(),
+            funcionarioModel.getSalario());
+    }
+
+    public static List<FuncionarioResponse> entityToDto(List<FuncionarioModel> funcionarioModel) {
+        return funcionarioModel
+            .stream()
+            .map(FuncionarioMapper::entityToDto)
+            .toList();
+    }
+
+    public static FuncionarioModel dtoToEntity(FuncionarioRequest funcionarioRequest) {
+        return new FuncionarioModel(
+            funcionarioRequest.nome(),
+            funcionarioRequest.cpf(),
+            funcionarioRequest.email(),
+            funcionarioRequest.salario());
+    }
+
 }
--- src/main/java/com/dam/orla/core/mapper/ProjetoMapper.java
+++ src/main/java/com/dam/orla/core/mapper/ProjetoMapper.java
@@ -1,4 +1,33 @@
 package com.dam.orla.core.mapper;
 
+import com.dam.orla.api.dto.request.ProjetoRequest;
+import com.dam.orla.api.dto.response.ProjetoResponse;
+import com.dam.orla.domain.model.ProjetoModel;
+
+import java.util.List;
+
 public class ProjetoMapper {
+    private ProjetoMapper() {
+    }
+
+    public static ProjetoResponse entityToDto(ProjetoModel projetoModel) {
+        return new ProjetoResponse(
+            projetoModel.getId(),
+            projetoModel.getNome(),
+            projetoModel.getDataCriacao(),
+            projetoModel.getFuncionarios()
+                .stream()
+                .map(FuncionarioMapper::entityToDto)
+                .toList());
+    }
+
+    public static List<ProjetoResponse> entityToDto(List<ProjetoModel> projetoModel) {
+        return projetoModel.stream().map(ProjetoMapper::entityToDto).toList();
+    }
+
+    public static ProjetoModel dtoToEntity(ProjetoRequest projetoRequest) {
+        return new ProjetoModel(
+            projetoRequest.nome(),
+            projetoRequest.dataCriacao());
+    }
 }
