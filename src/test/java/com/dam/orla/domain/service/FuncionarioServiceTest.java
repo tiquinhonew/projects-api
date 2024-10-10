49a2a4b739d3aeaad6071aba246444b837cb2fd1
Douglas Moraes <douglas.am@gmail.com>
2024-10-10 12:17:29 -0300

    Cobertura de testes

--- src/test/java/com/dam/orla/domain/service/FuncionarioServiceTest.java
+++ src/test/java/com/dam/orla/domain/service/FuncionarioServiceTest.java
@@ -1,4 +1,58 @@
+package com.dam.orla.domain.service;
+
+import com.dam.orla.api.dto.request.FuncionarioRequest;
+import com.dam.orla.domain.model.FuncionarioModel;
+import com.dam.orla.domain.repository.FuncionarioRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class FuncionarioServiceTest {
-  
+
+    @Mock
+    private FuncionarioRepository funcionarioRepository;
+
+    @InjectMocks
+    private FuncionarioService funcionarioService;
+
+
+    @Test
+    void deveCriarFuncionarioComSucesso() {
+        when(funcionarioRepository.save(any())).thenReturn(
+            new FuncionarioModel("Douglas Teste", "12345678900", "teste@email.com", BigDecimal.valueOf(1000)));
+
+        var funcionario = funcionarioService.criarFuncionario(
+            new FuncionarioRequest("Douglas Teste", "12345678900", "teste@email.com", BigDecimal.valueOf(1000)));
+
+        assertNotNull(funcionario);
+        assertEquals("Douglas Teste", funcionario.nome());
+    }
+
+    @Test
+    void deveListarFuncionariosComSucesso() {
+        when(funcionarioRepository.findAll()).thenReturn(
+            java.util.List.of(
+                new FuncionarioModel("Douglas Teste First", "12345678900", "teste@email.com", BigDecimal.valueOf(1000)),
+                new FuncionarioModel("Douglas Teste Last", "12345678900", "teste@email.com", BigDecimal.valueOf(1000))
+                             ));
+
+        var funcionarios = funcionarioService.listarFuncionarios();
+
+        assertNotNull(funcionarios);
+        assertEquals(2, funcionarios.size());
+
+        assertEquals("Douglas Teste First", funcionarios.getFirst().nome());
+        assertEquals("Douglas Teste Last", funcionarios.getLast().nome());
+    }
+
 }
--- src/test/java/com/dam/orla/domain/service/ProjetoServiceTest.java
+++ src/test/java/com/dam/orla/domain/service/ProjetoServiceTest.java
@@ -1,4 +1,84 @@
+package com.dam.orla.domain.service;
+
+import com.dam.orla.api.dto.request.ProjetoRequest;
+import com.dam.orla.domain.exception.EntidadeNaoEncontradaException;
+import com.dam.orla.domain.model.FuncionarioModel;
+import com.dam.orla.domain.model.ProjetoModel;
+import com.dam.orla.domain.repository.FuncionarioRepository;
+import com.dam.orla.domain.repository.ProjetoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class ProjetoServiceTest {
-  
+
+    @Mock
+    private ProjetoRepository projetoRepository;
+
+    @Mock
+    private FuncionarioRepository funcionarioRepository;
+
+    @InjectMocks
+    private ProjetoService projetoService;
+
+
+    @Test
+    void deveCriarProjetoComSucesso() {
+        ProjetoModel projeto = new ProjetoModel();
+        projeto.setNome("Projeto Teste");
+        projeto.setDataCriacao(LocalDate.now());
+
+        ProjetoRequest projetoModel = new ProjetoRequest("Projeto", LocalDate.now(), List.of(1L, 2L));
+
+        when(funcionarioRepository.findById(1L)).thenReturn(Optional.of(new FuncionarioModel("Douglas Teste", "12345678900", "teste@email.com", BigDecimal.valueOf(1000))));
+        when(funcionarioRepository.findById(2L)).thenReturn(Optional.of(new FuncionarioModel("Douglas Teste", "12345678900", "teste@email.com", BigDecimal.valueOf(1000))));
+        when(projetoRepository.save(any(ProjetoModel.class))).thenReturn(projeto);
+
+
+        var projetoCriado = projetoService.criarProjeto(projetoModel);
+
+        assertNotNull(projetoCriado);
+        assertEquals("Projeto Teste", projetoCriado.nome());
+    }
+
+    @Test
+    void deveLancarExcecaoAoCriarProjetoComFuncionarioInexistente() {
+        ProjetoRequest projetoModel = new ProjetoRequest("Projeto", LocalDate.now(), List.of(1L, 2L));
+
+        when(funcionarioRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(EntidadeNaoEncontradaException.class, () -> projetoService.criarProjeto(projetoModel));
+    }
+
+    @Test
+    void deveListarProjetosComSucesso() {
+        ProjetoModel projeto = new ProjetoModel();
+        projeto.setNome("Projeto Teste");
+        projeto.setDataCriacao(LocalDate.now());
+
+        when(projetoRepository.findAll()).thenReturn(List.of(projeto));
+
+        var projetos = projetoService.listarProjetos();
+
+        assertNotNull(projetos);
+        assertEquals(1, projetos.size());
+        assertEquals("Projeto Teste", projetos.getFirst().nome());
+    }
+
 }
