bffedd05cd308f7cb33907b4960eb2cd2d0e7635
Douglas Moraes <douglas.am@gmail.com>
2024-10-10 12:14:28 -0300

    Ajuste na service para validar se usuário ja existe

--- src/main/java/com/dam/orla/domain/service/FuncionarioService.java
+++ src/main/java/com/dam/orla/domain/service/FuncionarioService.java
@@ -1,5 +1,9 @@
 package com.dam.orla.domain.service;
 
+import com.dam.orla.api.dto.request.FuncionarioRequest;
+import com.dam.orla.api.dto.response.FuncionarioResponse;
+import com.dam.orla.core.mapper.FuncionarioMapper;
+import com.dam.orla.domain.exception.FuncionarioEmUsoException;
 import com.dam.orla.domain.model.FuncionarioModel;
 import com.dam.orla.domain.repository.FuncionarioRepository;
 import org.springframework.stereotype.Service;
@@ -15,12 +19,28 @@ public class FuncionarioService {
         this.funcionarioRepository = funcionarioRepository;
     }
 
-    public FuncionarioModel criarFuncionario(final FuncionarioModel funcionario) {
-        return funcionarioRepository.save(funcionario);
+    public FuncionarioResponse criarFuncionario(final FuncionarioRequest funcionario) {
+        final FuncionarioModel funcionarioModel = FuncionarioMapper.dtoToEntity(funcionario);
+        ifPresentThrowException(funcionario);
+        final FuncionarioModel novoFuncionario = funcionarioRepository.save(funcionarioModel);
+        return FuncionarioMapper.entityToDto(novoFuncionario);
     }
 
-    public List<FuncionarioModel> listarFuncionarios() {
-        return funcionarioRepository.findAll();
+    public List<FuncionarioResponse> listarFuncionarios() {
+        final List<FuncionarioModel> funcionarios = funcionarioRepository.findAll();
+        return FuncionarioMapper.entityToDto(funcionarios);
     }
+
+    private void ifPresentThrowException(final FuncionarioRequest funcionario) {
+        funcionarioRepository.findByCpf(funcionario.cpf())
+            .ifPresent(funcionarioExistente -> {
+                throw new FuncionarioEmUsoException("Já existe um funcionário cadastrado com o CPF informado");
+            });
+        funcionarioRepository.findByEmail(funcionario.email())
+            .ifPresent(funcionarioExistente -> {
+                throw new FuncionarioEmUsoException("Já existe um funcionário cadastrado com o e-mail informado");
+            });
+    }
+
 }
 
