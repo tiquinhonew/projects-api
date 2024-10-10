69f3235c8d0ae700885d2b33dbd66fd908d90ac9
Douglas Moraes <douglas.am@gmail.com>
2024-10-10 12:16:08 -0300

    Criando novos metodos para validar a existência de funcionario

--- src/main/java/com/dam/orla/domain/repository/FuncionarioRepository.java
+++ src/main/java/com/dam/orla/domain/repository/FuncionarioRepository.java
@@ -2,9 +2,17 @@ package com.dam.orla.domain.repository;
 
 import com.dam.orla.domain.model.FuncionarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<FuncionarioModel, Long> {
+
+    Optional<FuncionarioModel> findByCpf(String cpf);
+
+    Optional<FuncionarioModel> findByEmail(String email);
+
 }
 
