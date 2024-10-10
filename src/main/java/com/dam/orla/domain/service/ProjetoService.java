180f7436aa8fedb5457092303eae64e14deffb8e
Douglas Moraes <douglas.am@gmail.com>
2024-10-10 12:17:16 -0300

    Validando a existecia de funcionado antes de criar projeto

--- src/main/java/com/dam/orla/domain/service/ProjetoService.java
+++ src/main/java/com/dam/orla/domain/service/ProjetoService.java
@@ -1,26 +1,49 @@
 package com.dam.orla.domain.service;
 
+import com.dam.orla.api.dto.request.ProjetoRequest;
+import com.dam.orla.api.dto.response.ProjetoResponse;
+import com.dam.orla.core.mapper.ProjetoMapper;
+import com.dam.orla.domain.exception.FuncionarioNaoEncontradoException;
+import com.dam.orla.domain.model.FuncionarioModel;
 import com.dam.orla.domain.model.ProjetoModel;
+import com.dam.orla.domain.repository.FuncionarioRepository;
 import com.dam.orla.domain.repository.ProjetoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class ProjetoService {
 
     private final ProjetoRepository projetoRepository;
+    private final FuncionarioRepository funcionarioRepository;
 
-    public ProjetoService(final ProjetoRepository projetoRepository) {
+    public ProjetoService(final ProjetoRepository projetoRepository, final FuncionarioRepository funcionarioRepository) {
         this.projetoRepository = projetoRepository;
+        this.funcionarioRepository = funcionarioRepository;
     }
 
-    public ProjetoModel criarProjeto(final ProjetoModel projeto) {
-        return projetoRepository.save(projeto);
+    @Transactional
+    public ProjetoResponse criarProjeto(final ProjetoRequest projeto) {
+        final Set<FuncionarioModel> funcionarios = new HashSet<>();
+        projeto.funcionariosIds().forEach(funcionarioId -> {
+            funcionarioRepository.findById(funcionarioId).ifPresentOrElse(funcionarios::add, () -> {
+                throw new FuncionarioNaoEncontradoException(funcionarioId);
+            });
+        });
+
+        final ProjetoModel projetoModel = ProjetoMapper.dtoToEntity(projeto);
+        projetoModel.setFuncionarios(funcionarios);
+        final ProjetoModel novoProjeto = projetoRepository.save(projetoModel);
+        return ProjetoMapper.entityToDto(novoProjeto);
     }
 
-    public List<ProjetoModel> listarProjetos() {
-        return projetoRepository.findAll();
+    public List<ProjetoResponse> listarProjetos() {
+        final List<ProjetoModel> projetos = projetoRepository.findAll();
+        return ProjetoMapper.entityToDto(projetos);
     }
 }
 
